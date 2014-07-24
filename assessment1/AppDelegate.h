7922f0a3367f7537a3820f0f546e309cdc746ab4
Efrén Reyes <efren.reyes@globant.com>
2014-07-24 13:44:26 -0500

    Initial Commit

---
+++ assessment1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  assessment1
+//
+//  Created by Efrén Reyes Torres on 7/24/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ assessment1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  assessment1
+//
+//  Created by Efrén Reyes Torres on 7/24/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
